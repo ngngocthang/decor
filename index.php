7fdb3dc0bbd1be572747951be74856f04eb36eb9
Thang Nguyen <ngngocthang2016@gmail.com>
2018-12-23 00:15:05 +0700

    herokuapp1

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+	<title> Decor Hà Nội </title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">  
+	<script type="text/javascript" src="vendor/bootstrap.js"></script>
+ 	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+ 	<script src='https://cdn.rawgit.com/matthieua/WOW/1.0.1/dist/wow.min.js'></script>
+	<link rel="stylesheet" href="vendor/bootstrap.css">
+	<link rel="stylesheet" href="vendor/font-awesome.css">
+	<link rel="stylesheet" href="vendor/animate.css">
+ 	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
 <?php include_once("home.html"); ?>
+
+	<script src="vendor/isotope.pkgd.min.js"></script>
+ 	<script type="text/javascript" src="1.js"></script>
+</body>
+</html>
