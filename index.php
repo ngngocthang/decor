50e9ded9b8715c61df46d15c7bf8c3f78247cf48
Thang Nguyen <ngngocthang2016@gmail.com>
2018-12-23 00:50:56 +0700

    :((

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<?php include_once("home.html"); ?>
+<!DOCTYPE html>
+<html lang="en"><head>
+	<title> Decor Hà Nội </title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">  
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+ 	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+ 	<script src='https://cdn.rawgit.com/matthieua/WOW/1.0.1/dist/wow.min.js'></script>
+	<link rel="stylesheet" href="<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+	<link href="https://fastcdn.org/Animate.css/3.4.0/animate.min.css" rel="stylesheet">
+ 	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+	<?php include_once("home.html"); ?>
+
+<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
+ 	<script type="text/javascript" src="1.js"></script>
+ </body>
+ </html>
